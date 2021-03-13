a7819a30e678eddbfb6ffb951b6b6f434f9e650e
holmen1 <holmen1@gmail.com>
2021-03-13 16:47:27 +0100

    Add serialization

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 Test of FastAPI https://fastapi.tiangolo.com/
 
+Dockerize numpy calculation, call from C#
+
+Passing matrix A: [[1.0, 0.032], [0.032, 1.0]]
+
+Factorize A = U S Vh
+
+Check that A == A' = S[0] U[:,0] Vh[0,:] + S[1] U[:,1] Vh[1,:] 
+
+
+
+## api
+
 /api$ uvicorn main:app --reload
 
 $ curl -X POST localhost:8000/items/ -H "accept: application/json" -H "Content-Type: application/json" -d "{\"name\":\"Foo\",\"A\":[[1.0, 0.032], [0.032, 1.0]]}"
@@ -14,9 +26,11 @@ $ curl -X POST localhost:8000/items/ -H "accept: application/json" -H "Content-T
 
 $ curl -X POST localhost:8080/items/ -H "accept: application/json" -H "Content-Type: application/json" -d "{\"name\":\"Foo\",\"A\":[[1.0, 0.032], [0.032, 1.0]]}"
 
+{"U":[[-0.7071067811865472,-0.7071067811865478],[-0.707106781186548,0.7071067811865472]],"S":[1.0319999999999998,0.968],"Vh":[[-0.7071067811865472,-0.7071067811865479],[-0.7071067811865479,0.7071067811865472]]}
+
 ## console
 
 /console$ dotnet run
 
-{"U":[[-0.7071067811865472,-0.7071067811865478],[-0.707106781186548,0.7071067811865472]],"S":[1.0319999999999998,0.968],"Vh":[[-0.7071067811865472,-0.7071067811865479],[-0.7071067811865479,0.7071067811865472]]}
+
 
--- console/Program.cs
+++ console/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace console
@@ -16,12 +18,35 @@ namespace console
         private static async Task Factorize()
         {
             string jparams = "{\"name\":\"Foo\",\"A\":[[1.0, 0.032], [0.032, 1.0]]}";
+            Console.WriteLine(jparams);
+            Console.WriteLine("Factorize A = U S Vh");
+
             client.BaseAddress = new Uri("http://localhost:8080/items/");
             using var content = new StringContent(jparams, Encoding.UTF8, "application/json");
-            var response = await client.PostAsync((Uri)null, content);
-            var factors = await response.Content.ReadAsStringAsync();
 
-            Console.Write(factors);
+            var response = await client.PostAsync((Uri)null, content);
+            using var contentStream = await response.Content.ReadAsStreamAsync();
+            var jfactors = await JsonSerializer.DeserializeAsync<Factors>(contentStream);
+
+            // Control, multiply factors A = S[0] U[:,0] Vh[0,:] + S[1] U[:,1] Vh[1,:] 
+            double a11 = jfactors.S[0] * jfactors.U[0][0] * jfactors.Vh[0][0]
+             + jfactors.S[1] * jfactors.U[0][1] * jfactors.Vh[1][0];
+            double a12 = jfactors.S[0] * jfactors.U[0][0] * jfactors.Vh[0][1]
+             + jfactors.S[1] * jfactors.U[0][1] * jfactors.Vh[1][1];
+            double a21 = jfactors.S[0] * jfactors.U[1][0] * jfactors.Vh[0][0]
+             + jfactors.S[1] * jfactors.U[1][1] * jfactors.Vh[1][0];
+            double a22 = jfactors.S[0] * jfactors.U[1][0] * jfactors.Vh[0][1]
+             + jfactors.S[1] * jfactors.U[1][1] * jfactors.Vh[1][1];
+
+            Console.WriteLine($"a11: {Math.Round(a11, 3)} \t\t a12: {Math.Round(a12, 3)}");
+            Console.WriteLine($"a21: {Math.Round(a21, 3)} \t a22: {Math.Round(a22, 3)}");
+        }
+
+        public class Factors
+        {
+            public double[][] U { get; set; }
+            public double[] S { get; set; }
+            public double[][] Vh { get; set; }
         }
     }
 }
