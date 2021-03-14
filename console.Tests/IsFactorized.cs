64b2929eae489e1443493e58c5ab2f84e7546fd8
holmen1 <holmen1@gmail.com>
2021-03-14 10:40:13 +0100

    Add unit test

---
+++ console.Tests/IsFactorized.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using console;
+using System;
+using System.Threading.Tasks;
+
+namespace console.UnitTests.Services
+{
+    [TestFixture]
+    public class IsFactorized
+    {
+        private Factory _factory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            string jparams = "{\"name\":\"Foo\",\"A\":[[1.0, 0.032], [0.032, 1.0]]}";
+            _factory = new Factory(jparams);
+        }
+
+        [Test]
+        public async Task IsDiffLessThanTolReturnTrue()
+        {
+            var a = _factory.GetFactors();
+            Factory.Factors factors = await a;
+
+            // Control, multiply factors A = S[0] U[:,0] Vh[0,:] + S[1] U[:,1] Vh[1,:] 
+            double a11 = factors.S[0] * factors.U[0][0] * factors.Vh[0][0]
+             + factors.S[1] * factors.U[0][1] * factors.Vh[1][0];
+            double a12 = factors.S[0] * factors.U[0][0] * factors.Vh[0][1]
+             + factors.S[1] * factors.U[0][1] * factors.Vh[1][1];
+            double a21 = factors.S[0] * factors.U[1][0] * factors.Vh[0][0]
+             + factors.S[1] * factors.U[1][1] * factors.Vh[1][0];
+            double a22 = factors.S[0] * factors.U[1][0] * factors.Vh[0][1]
+             + factors.S[1] * factors.U[1][1] * factors.Vh[1][1];
+
+            double diff = Math.Abs(a11 - 1.0) + Math.Abs(a12 - 0.032) +
+                            Math.Abs(a21 - 0.032) + Math.Abs(a22 - 1.0);
+            Assert.Less(diff, 1E-3, "1 should not be prime");
+        }
+    }
+}
---
+++ console/Factory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace console
+{
+    public class Factory
+    {
+        private string _jparams;
+        private static readonly HttpClient client = new HttpClient();
+        
+        public Factory(string jparams)
+        {
+            _jparams = jparams;
+            client.BaseAddress = new Uri("http://localhost:8080/items/");
+        }
+
+        
+
+        public async Task<string> GetJsonFactors()
+        {
+            using var content = new StringContent(_jparams, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync((Uri)null, content);
+            using var contentStream = await response.Content.ReadAsStreamAsync();
+            var jfactors = await JsonSerializer.DeserializeAsync<Factors>(contentStream);
+
+            var json = JsonSerializer.Serialize(jfactors);
+            return json;
+        }
+
+        public async Task<Factors> GetFactors()
+        {
+            using var content = new StringContent(_jparams, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync((Uri)null, content);
+            using var contentStream = await response.Content.ReadAsStreamAsync();
+            var jfactors = await JsonSerializer.DeserializeAsync<Factors>(contentStream);
+
+            return jfactors;
+        }
+
+        public class Factors
+        {
+            public double[][] U { get; set; }
+            public double[] S { get; set; }
+            public double[][] Vh { get; set; }
+        }
+    }
+}
